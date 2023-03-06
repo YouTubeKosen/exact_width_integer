f1f6dc93fedcdb541149b896962736802e4fc540
YouTubeKosen <private.kosen@gmail.com>
2023-03-06 21:26:45 +0900

    add sum and sum_f

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-    int hoge;
-    float fuga = 3.4e38;
-    hoge = fuga;
-    printf("%d\n", hoge);
+    int hoge=0, fuga=1, sum;
+    float hoge_f=0, fuga_f=1, sum_f;
+
+    sum = hoge + fuga;
+    sum_f = hoge_f + fuga_f;
+    printf("%d, %f\n", sum, sum_f);
 }
